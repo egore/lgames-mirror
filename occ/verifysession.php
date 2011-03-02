bafb1acf66705afca5f69fc8ab4130e877a02213
kulkanie <kulkanie@b357a508-0cf7-4c0e-b265-6d29fad4a9a1>
2011-03-02 10:13:09 +0000

    tagged ltris release 1.0.15

--- occ/verifysession.php
+++
@@ -1,7 +0,0 @@
-<? /* Verify running session (and return to login page if not logged in). */
-include 'session.php';
-if (isset($_SESSION['uid']))
-	$uid=$_SESSION['uid'];
-else
-	header('location:index.php');
-?>
