f5eefbdc7c0f65ce9004a4959d8f04c4fb7cc7cb
kulkanie <kulkanie@b357a508-0cf7-4c0e-b265-6d29fad4a9a1>
2012-02-03 19:30:56 +0000

    tagged release lmarbles 1.0.8

--- occ/images/beholder/credits.php
+++
@@ -1,2 +0,0 @@
-<? $credits='The chessmen graphics were taken from <A class="sublink" href="http://sourceforge.net/projects/webchess/">WebChess</A>.';?>
-
--- occ/images/default/credits.php
+++
@@ -1,2 +0,0 @@
-<? $credits='The chessmen graphics were taken from <A class="tiny" href="http://www.tommasovitale.it/my/wcg">WCG</A> with kind permission of Tommaso Vitale.';?>
-
